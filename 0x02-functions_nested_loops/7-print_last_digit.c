54848735990fbe0ab8bd3a6a495e924b6ba5a875
Chukwuebuka Ezenwobodo <princejason109@gmail.com>
2023-08-15 09:28:47 -0700

    alx low level programming functions and nexted loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
 * print_last_digit - prints the last digit of a number
 * @n: the int to extract the last digit from
@@ -13,12 +14,12 @@ int print_last_digit(int n)
 		n = -n;
 
 	a = n % 10;
-		
+			
 	if (a < 0)
 		a = -a;
-	
+			
 	_putchar(a + '0');
-	
-	return (a);
 		
+	return (a);
+			
 }
