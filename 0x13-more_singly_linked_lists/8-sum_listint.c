7fcac6fb02b6892fd6403be6141feb4a0e0ccb10
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-09-26 23:28:05 +0100

    wip

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+int sum_listint(listint_t *head)
+{
+	int total = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		total += temp->n;
+		temp = temp->next;
+	}
+
+	return (total);
+}
